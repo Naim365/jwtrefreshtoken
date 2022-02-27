bd1faa94bfb4a6bcdca0c43ae9528328bab75a4a
Naim365 <shahjadanaimuddin@gmail.com>
2022-02-27 18:53:17 +0600

    access and refress token work successfully

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+  {
+    username: 'Naim',
+    password: '123456',
+    Description: 'Post 1'
+  },
+  {
+    username: 'Sazid',
+    password: '78910',
+    Description: 'Post 2'
+  }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
+})
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+  console.log(`listen to port ${port}`)
+})
